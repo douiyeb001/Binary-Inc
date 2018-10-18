1edbd0917c73749ec1a8a922d569bb31434a8b73
PetervH <peter.van.holland@hva.nl>
2018-10-18 16:34:34 +0200

    [Add] Movie in unity

---
+++ Assets/Scripts/MovieHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MovieHandler : MonoBehaviour {
+
+    VideoPlayer movie;
+    public long frames;
+	// Use this for initialization
+	void Start () {
+        movie = GetComponent<VideoPlayer>();
+
+        movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frames = movie.frame;
+        if (movie.frame == 34)
+            movie.Play();
+	}
+}
