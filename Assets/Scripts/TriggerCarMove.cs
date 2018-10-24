8ed788d89f88a663cd0456cac2a6fa6aff782506
Unknown <Bilal.Douiyeb@hva.nl>
2018-10-24 11:39:44 +0200

    [EDIT] Fixing the car movement

---
+++ Assets/Scripts/TriggerCarMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCarMove: MonoBehaviour {
+
+    private EnableLights[] lights;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        lights = GetComponentsInChildren<EnableLights>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.activeSelf == true)
+        {
+            foreach (EnableLights _light in lights)
+            {
+                _light.GetComponent<Light>().enabled = true;
+            }
+            
+        }
+         this.transform.position += new Vector3(speed, 0, 0);
+       
+    }
+}
