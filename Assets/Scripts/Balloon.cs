b22d100474bc615aec5f64b469dd2853a786cd94
Peter <peter.van.holland@hva.nl>
2018-10-24 17:20:51 +0200

    [Add] Special effect with particle system & Created balloons

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+    public float difference;
+    public float y;
+    float speed = 0.003f;
+    float turnSpeed = 1;
+    // Use this for initialization
+    void Start () {
+        
+        transform.position = new Vector3(Random.Range(-30, 30), -65+Random.Range(0,8), Random.Range(-30, 30));
+        y = gameObject.transform.position.y;
+        gameObject.transform.position += new Vector3(0, Random.Range(-0.25f, 0.25f), 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        difference = y - gameObject.transform.position.y;
+        transform.position += new Vector3(0, speed, 0);
+        if (difference < -0.3 && speed > 0)
+        {
+            speed = speed * -turnSpeed;
+        }
+        if (difference > 0.3 && speed < 0)
+        {
+            speed = speed * -turnSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour {
+    public Material[] material;
+    Renderer rend;
+
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Box")
+        {
+            rend.sharedMaterial = material[Random.Range(1,material.Length)];
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        rend.sharedMaterial = material[0];
+    }
+}
