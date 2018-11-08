957222a31508a7e750a20f5d15eb171c1027db0a
Unknown <Bilal.Douiyeb@hva.nl>
2018-11-08 14:40:36 +0100

    [EDIT] Fix scenes

---
+++ Assets/Scripts/Movement scripts/BlinkingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingScript : MonoBehaviour {
+    private int frame;
+     public Image img;
+	// Use this for initialization
+	void Start () {
+        img = FindObjectOfType<Image>();
+        img.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        frame++;
+        if (frame ==  90)
+        {
+            img.enabled = true;
+            frame = 0;
+        }
+        else img.enabled = false;
+	}
+}
