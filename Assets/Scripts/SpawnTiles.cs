6dc2b97a1c8edebd1f5fd4056ef93c02abf78d18
Unknown <peter.van.holland@hva.nl>
2018-12-04 10:31:54 +0100

    alarm

---
+++ Assets/Scripts/SpawnTiles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTiles : MonoBehaviour {
+    public GameObject tile;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Instantiate(tile, transform.position, transform.rotation);
+	}
+    
+
+}
