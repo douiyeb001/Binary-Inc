c2a578c21c4a188d76548ed64ee5a7f569a9d6c4
Unknown <peter.van.holland@hva.nl>
2018-12-09 22:15:54 +0100

    Scene switch up. Portal naar de "insider"

---
+++ Assets/Scripts/CharacterSwitchHoloToParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitchHoloToParticle : MonoBehaviour {
+    public GameObject charParticle;
+	// Use this for initialization
+	void Start () {
+        //charParticle = GameObject.Find("WalkingCharParticle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision);
+        
+        charParticle.SetActive(true);
+        gameObject.SetActive(false);
+        
+    }
+}
