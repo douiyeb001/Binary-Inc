a20ec499563634dbafff7052567e983d4a15d83a
Unknown <peter.van.holland@hva.nl>
2018-12-19 11:55:27 +0100

    ready for merge

--- Assets/Scripts/SpatialAudio.cs
+++ Assets/Scripts/SpatialAudio.cs
@@ -7,19 +7,14 @@ public class SpatialAudio : MonoBehaviour {
     public Transform player;
     public Camera cam;
     public float volume;
+    float sum;
     public float difference;
     float constant = 5;
     float maxVolume,minVolume,audioVolume;
-    public float rot;
-    float x, z;
-    float cheapX, cheapZ;
-    public float angle,cheapAngle;
-    public float angleDifference;
     bool inView = false;
 
 	// Use this for initialization
 	void Start () {
-		
         maxVolume = .15f;
         minVolume = .03f;
         audioVolume=.15f;
@@ -29,13 +24,14 @@ public class SpatialAudio : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         
-     
         difference = Vector3.Distance(transform.position, player.position);
+        
         if (difference<0)
             difference = difference*-1;
-        if (maxVolume> constant / difference)
+        sum = constant / difference;
+        if (maxVolume> sum)
         {
-            GetComponent<AudioSource>().volume = constant / difference;
+            GetComponent<AudioSource>().volume = sum;
         }
         else
         {
@@ -62,9 +58,9 @@ public class SpatialAudio : MonoBehaviour {
         {
             audioVolume -= .002f;
         }
-        GetComponent<AudioSource>().volume = audioVolume;
-
-
+        if(maxVolume>=sum && maxVolume >=audioVolume)
+            GetComponent<AudioSource>().volume = audioVolume;
+        
 
     }
 }
