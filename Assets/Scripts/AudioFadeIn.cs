d9e7da5230841a824186b63af4de15aca6deef20
Unknown <Bilal.Douiyeb@hva.nl>
2019-01-22 00:33:43 +0100

    [small changes]

---
+++ Assets/Scripts/AudioFadeIn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class AudioFadeIn : MonoBehaviour
+{
+    [SerializeField]
+    private int m_FadeInTime = 10;
+    public AudioSource m_AudioSource;
+
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void Update()
+    {
+        if (m_AudioSource.volume < 1)
+        {
+            m_AudioSource.volume = m_AudioSource.volume + (Time.deltaTime / (m_FadeInTime + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
